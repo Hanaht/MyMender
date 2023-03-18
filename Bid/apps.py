541732666e1246fdec799279391d7b4510b4a0c3
hanah <hanahtesfahun@gmail.com>
2023-03-18 14:40:45 +0300

    requirement.txt added

--- Bid/apps.py
+++ Bid/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
-class AuthConfig(AppConfig):
+
+class BidConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Auth'
+    name = 'Bid'
